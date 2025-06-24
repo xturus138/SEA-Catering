62709c3b66967c5e625cacc9ae6893e4e72e0871
xturus138 <radityaramadhan.138@gmail.com>
2025-06-24 11:59:31 +0700

    feat: implement user session check to redirect to BaseActivity if logged in

--- app/src/main/java/com/example/seacatering/ui/MainActivity.kt
+++ app/src/main/java/com/example/seacatering/ui/MainActivity.kt
@@ -6,12 +6,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
 import androidx.viewpager2.widget.ViewPager2
 import com.example.seacatering.R
 import com.example.seacatering.adapter.LandingPagerAdapter
+import com.example.seacatering.data.DataStoreManager
 import com.example.seacatering.databinding.ActivityMainBinding
+import com.example.seacatering.ui.auth.login.LoginFragment
 import com.example.seacatering.utils.hideSystemBars
 import com.tbuonomo.viewpagerdotsindicator.DotsIndicator
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,12 +24,24 @@ class MainActivity : AppCompatActivity() {
     private lateinit var dotsIndicator: DotsIndicator
     private lateinit var binding: ActivityMainBinding
     private var hasNavigatedToAuth = false
+    private lateinit var dataStoreManager: DataStoreManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        dataStoreManager = DataStoreManager(this)
+
+        lifecycleScope.launch {
+            val user = dataStoreManager.userData.first()
+            if (user != null) {
+                startActivity(Intent(this@MainActivity, BaseActivity::class.java))
+                finish()
+            } else {
+                setContentView(binding.root)
+            }
+        }
+
         hideSystemBars()
         viewPager = binding.viewPager2
         dotsIndicator = binding.dotsIndicator
